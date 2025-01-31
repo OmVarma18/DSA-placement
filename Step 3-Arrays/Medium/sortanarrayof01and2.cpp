d5d6d59ad65d9fe60f5f4ba7a570f6987d6305b0
OmVarma9 <omvarma369@gmail.com>
2025-01-31 23:16:08 +0530

    sort arr of 0 1 2

---
+++ Step 3-Arrays/Medium/sortanarrayof01and2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter the size of the array ";
+    short n,t;
+    cin>>n;
+    short arr[n];
+    for(short i=0;i<n;i++) cin>>arr[i];
+
+return 0;
+}
