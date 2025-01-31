7705823b55df82503b9796fe4d7c30bfd477aeff
OmVarma9 <omvarma369@gmail.com>
2025-01-31 22:49:17 +0530

    2 sum hashing

--- Step 3-Arrays/Medium/2sum.cpp
+++ Step 3-Arrays/Medium/2sum.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
-#include <math.h>
+#include <map>
 using namespace std;
+// brute is O(n2)
+// better hashing
+int main(){
+    cout<<"Enter the size of the array";
+    short n,t;
+    cin>>n;
+    short arr[n];
+    for(short i=0;i<n;i++) cin>>arr[i];
+    cout<<"target";
+    cin>>t;
+    map<int,int> numm;
+
+    for(short i=0;i<n;i++){
+        short comp=t-arr[i];
+
+        if(numm.find(comp)!=numm.end()){
+            cout<< numm[comp]<<" "<<i;
+            break;
+        }
+        numm[arr[i]];
+    }
+    return 0;
+}
