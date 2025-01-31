586922c4258a5a2c10d5dc0ffafd7e815e86e8eb
OmVarma9 <omvarma369@gmail.com>
2025-01-31 23:07:56 +0530

    2 sum 2 pointers

--- Step 3-Arrays/Medium/2sum.cpp
+++ Step 3-Arrays/Medium/2sum.cpp
@@ -4,23 +4,36 @@ using namespace std;
 // brute is O(n2)
 // better hashing
 int main(){
-    cout<<"Enter the size of the array";
+    cout<<"Enter the size of the array ";
     short n,t;
     cin>>n;
     short arr[n];
     for(short i=0;i<n;i++) cin>>arr[i];
-    cout<<"target";
+    cout<<"target ";
     cin>>t;
-    map<int,int> numm;
+    // map<int,int> numm;
 
-    for(short i=0;i<n;i++){
-        short comp=t-arr[i];
+    // for(short i=0;i<n;i++){
+    //     short comp=t-arr[i];
 
-        if(numm.find(comp)!=numm.end()){
-            cout<< numm[comp]<<" "<<i;
+    //     if(numm.find(comp)!=numm.end()){
+    //         cout<< numm[comp]<<" "<<i;
+    //         break;
+    //     }
+    //     numm[arr[i]];
+    // }
+
+    //optimal
+    short left=0,right=n-1;
+    while(left<right){
+        if(arr[left]+arr[right]==t){
+            cout<<left<<" "<<right;
             break;
         }
-        numm[arr[i]];
+        else if(arr[left]+arr[right]<t)
+            left++;
+        else
+          right--;
     }
     return 0;
 }
