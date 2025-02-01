b442fc95a7a719af1da390477beeb8419f6dc56f
OmVarma9 <omvarma369@gmail.com>
2025-02-01 22:19:07 +0530

    optimal

--- Step 3-Arrays/Medium/sortanarrayof01and2.cpp
+++ Step 3-Arrays/Medium/sortanarrayof01and2.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 /*brute way is NlogN quick sort or merge
 better is counter fro 0 1 and 2 then re ad to the orignal array
+optimal dutch national flog
+ 3 ponter low mid high
 */
 int main(){
     cout<<"Enter the size of the array ";
@@ -10,6 +12,25 @@ int main(){
     short arr[n];
     for(short i=0;i<n;i++) cin>>arr[i];
 
+int low=0,mid=0,high=n-1;
+
+while(!(mid>high)){
+    if(arr[mid]==0){
+        swap(arr[low],arr[mid]);
+        mid++;
+        low++;
+    }
+    else if(arr[mid]==1){
+        mid++;
+    }
+    else{
+        swap(arr[high],arr[mid]);
+        high--;
+    }
+
+}
+
+for(short i=0;i<n;i++) cout<<arr[i]<<" ";
 
 return 0;
 }
