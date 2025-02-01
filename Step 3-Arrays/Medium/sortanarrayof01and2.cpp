6208b21bb0782492380e19affa3a1b8a504d40f7
OmVarma9 <omvarma369@gmail.com>
2025-02-01 22:00:21 +0530

    brutte and better

--- Step 3-Arrays/Medium/sortanarrayof01and2.cpp
+++ Step 3-Arrays/Medium/sortanarrayof01and2.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-
+/*brute way is NlogN quick sort or merge
+better is counter fro 0 1 and 2 then re ad to the orignal array
+*/
 int main(){
     cout<<"Enter the size of the array ";
     short n,t;
     cin>>n;
     short arr[n];
     for(short i=0;i<n;i++) cin>>arr[i];
+
 
 return 0;
 }
