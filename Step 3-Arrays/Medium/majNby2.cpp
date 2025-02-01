22c63c8c584591d70a34438bc687fe61f06ff24f
OmVarma9 <omvarma369@gmail.com>
2025-02-01 22:38:15 +0530

    Better

--- Step 3-Arrays/Medium/majNby2.cpp
+++ Step 3-Arrays/Medium/majNby2.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <iostream>]
+#include <map>
 using namespace std;
 /*
 Brute-pick up one element then itterate the entire elemet O(n2)
@@ -11,17 +12,27 @@ int main(){
     short n;
     short nums[n];
     for(short i;i<n;i++) cin>>nums[i];
-    int majorityElement = -1;
-    for (int i = 0; i < n; ++i) {
-        int count = 0;
-        for (int j = 0; j < n; ++j) {
-            if (nums[j] == nums[i]) {
-                count++;
-            }
-        }
-        if (count > n / 2) {
-            majorityElement = nums[i];
-            break;  // Exit as soon as the majority element is found
+    //brute
+    // int majorityElement = -1;
+    // for (int i = 0; i < n; ++i) {
+    //     int count = 0;
+    //     for (int j = 0; j < n; ++j) {
+    //         if (nums[j] == nums[i]) {
+    //             count++;
+    //         }
+    //     }
+    //     if (count > n / 2) {
+    //         majorityElement = nums[i];
+    //         break;  // Exit as soon as the majority element is found
+    //     }
+    // }
+    //Better
+    map<short,short> mpp;
+    for(short i=0;i<n;i++){
+        mpp[nums[i]]++;
+    if(mpp[nums[i]]>>n/2){
+        cout<<nums[i];
+        break;
         }
     }
     return 0;
